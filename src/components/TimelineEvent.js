8924cd6741681055519e4bd0cc098c3296ae1868
amyesh <amyesh08@gmail.com>
2019-06-10 16:30:25 -0700

    replace class with className

--- src/components/TimelineEvent.js
+++ src/components/TimelineEvent.js
@@ -4,10 +4,10 @@ import Timestamp from './Timestamp';
 
 const TimelineEvent = (props) => {
   return (
-    <section class="timeline timeline-event">
-      <span class="event-person">{ props.person }</span>
-      <span class="event-time"><Timestamp time={ props.timeStamp } /></span>
-      <p class="event-status">
+    <section className="timeline timeline-event">
+      <span className="event-person">{ props.person }</span>
+      <span className="event-time"><Timestamp time={ props.timeStamp } /></span>
+      <p className="event-status">
         { props.status }
       </p>
     </section>
