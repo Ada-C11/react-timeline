7920e1cb977b93869849e53e905466d0657aad33
Angela <angela.ohh@gmail.com>
2019-06-10 15:09:47 -0700

    parsed JSON into array and mapped to display in jsx

--- src/App.js
+++ src/App.js
@@ -1,20 +1,35 @@
 import React, { Component } from 'react';
 import './App.css';
 import timelineData from './data/timeline.json';
-
 import Timeline from './components/Timeline';
 
 class App extends Component {
   render() {
-    console.log(timelineData);
 
-    // Customize the code below
+    const postingData = timelineData.events.map ((post) => {
+      return (
+        {person: post.person, status: post.status, timeStamp: post.timeStamp}
+      )
+    });
+
+    const displayPost = postingData.map ((post, i) => {
+        return (
+          <li key={i}>
+            <Timeline event={ post } />
+          </li>
+        );
+      });
+    
+
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Application title</h1>
         </header>
         <main className="App-main">
+          <ul>
+            {displayPost}
+          </ul>
         </main>
       </div>
     );
