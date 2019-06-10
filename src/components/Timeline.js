6c006fcb7836524e650c9d40e41372f6b663907e
Kimberly-Fasbender <kimberly.fasbender@gmail.com>
2019-06-10 16:23:44 -0700

    updated Timeline component

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -2,9 +2,26 @@ import React from 'react';
 import './Timeline.css';
 import TimelineEvent from './TimelineEvent';
 
-const Timeline = () => {
-  // Fill in your code here
-  return;
+const Timeline = (props) => {
+
+  const timelineEvents = props.events
+
+  const timelineEventComponents = timelineEvents.map ((timelineEvent, i) => {
+    return (
+      <div key={i} >
+        <TimelineEvent 
+          person={timelineEvent.person} 
+          timestamp={timelineEvent.timestamp} 
+          status={timelineEvent.status} />
+      </div>
+    );
+  });
+ 
+  return (
+    <div className='timeline'>
+      {timelineEventComponents}
+    </div>
+  );
 }
 
 export default Timeline;
