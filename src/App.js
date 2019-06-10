dcb71085442ea3a4afa99a17d8c93db8b0c6d237
Heather Izumi <hkizumi@gmail.com>
2019-06-10 15:58:13 -0700

    Added props to TimelineEvent and implemented Timestamp component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
 import timelineData from './data/timeline.json';
+import TimelineEvent from './components/TimelineEvent';
+
 
 import Timeline from './components/Timeline';
 
@@ -8,13 +10,19 @@ class App extends Component {
   render() {
     console.log(timelineData);
 
+
+  
+
     // Customize the code below
-    return (
+    return(
       <div className="App">
         <header className="App-header">
-          <h1 className="App-title">Application title</h1>
+          <h1 className="App-title">Ada Lovelace's social media feed</h1>
         </header>
         <main className="App-main">
+          <section>
+            <TimelineEvent person="heather" status="busy" timestamp="2018-05-18T22:12:03Z" />
+          </section>
         </main>
       </div>
     );
--- src/components/TimelineEvent.js
+++ src/components/TimelineEvent.js
@@ -2,9 +2,28 @@ import React from 'react';
 import './TimelineEvent.css';
 import Timestamp from './Timestamp';
 
-const TimelineEvent = () => {
-  // Fill in your code here
-  return;
+const TimelineEvent = (props) => {
+
+  // const time = <Timestamp time="2018-05-18T22:12:03Z" />
+
+
+  return(
+    <section className="timeline-event">
+      <div className="event-person">
+        {props.person}
+      </div>
+
+      <div className="event-status">
+        {props.status}
+      </div>
+
+      <div className="event-time">
+        <Timestamp time={props.timestamp} />
+      </div>
+
+    </section>
+
+  );
 }
 
 export default TimelineEvent;
