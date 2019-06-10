4bae2d635b65180785ac78a104475958df344cd4
Alexandria McCarthy <alexandria.mccarthy77@gmail.com>
2019-06-10 15:50:44 -0700

    accessed json info to get title

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <h1 className="App-title">Application title</h1>
+          <h1 className="App-title">{ timelineData["person"]}'s Social Media Feed</h1>
         </header>
         <main className="App-main">
           <Timeline />
