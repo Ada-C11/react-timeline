206009d662b12c3477e8cc95ec8fe50332f52565
Elise Pham <elise.pham88@gmail.com>
2019-06-10 15:45:10 -0700

    pulled all data correctly

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -2,9 +2,19 @@ import React from 'react';
 import './Timeline.css';
 import TimelineEvent from './TimelineEvent';
 
-const Timeline = () => {
-  // Fill in your code here
-  return;
+const Timeline = (props) => {
+  console.log(props);
+  const individualEvent = props.events.map( (event, i) => {
+    return (
+      <p key={i}><TimelineEvent person={event.person} status={event.status} timestamp={event.timeStamp} /></p>
+    );
+  })
+
+  return (
+    <section className="timeline">
+      { individualEvent }
+    </section>
+  );
 }
 
 export default Timeline;
