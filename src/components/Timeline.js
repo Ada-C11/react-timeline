109f099702090a39c6ded8fd8f845285a0a1a0d9
Jessica <jessica.homet@gmail.com>
2019-06-10 15:28:21 -0700

    testing comment

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './Timeline.css';
 import TimelineEvent from './TimelineEvent';
-
+// testing
 const Timeline = (props) => {
   const eventComponents = props.events.map((event, i) => {
     return (
