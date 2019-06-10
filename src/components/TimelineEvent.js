0e0948f18747c773f501bf57951e0453d78b8311
Jillianne Ramirez <jillianne.ramirez@gmail.com>
2019-06-10 15:51:53 -0700

    3 props included in timelineevent component

--- src/components/TimelineEvent.js
+++ src/components/TimelineEvent.js
@@ -2,9 +2,16 @@ import React from 'react';
 import './TimelineEvent.css';
 import Timestamp from './Timestamp';
 
-const TimelineEvent = () => {
-  // Fill in your code here
-  return;
+const TimelineEvent = (props) => {
+  return (
+    <div className="TimelineEvent">
+      <header className="Student-header">
+        <p>{props.person}</p>
+        <p>{props.status}</p>
+        <p><Timestamp /></p>
+      </header>
+    </div>
+  );
 }
 
 export default TimelineEvent;
