a90369bf8a16c516fa341c6a1c15809e80660b92
Angela <angela.ohh@gmail.com>
2019-06-10 15:08:46 -0700

    added person status and time props

--- src/components/TimelineEvent.js
+++ src/components/TimelineEvent.js
@@ -2,9 +2,17 @@ import React from 'react';
 import './TimelineEvent.css';
 import Timestamp from './Timestamp';
 
-const TimelineEvent = () => {
-  // Fill in your code here
-  return;
-}
+const TimelineEvent = (props) => {
+
+  const time = <Timestamp time={ new Date } />
+
+  return(
+    <section>
+      <h1>{props.person}</h1>
+      <h3>{props.status}</h3>
+      <h6>{time}</h6>
+    </section>
+  );
+};
 
 export default TimelineEvent;
