bccc77f05eafb207596947fc14b63a9febb799e1
Angela <angela.ohh@gmail.com>
2019-06-10 15:09:09 -0700

    added event prop

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -2,9 +2,12 @@ import React from 'react';
 import './Timeline.css';
 import TimelineEvent from './TimelineEvent';
 
-const Timeline = () => {
-  // Fill in your code here
-  return;
+
+const Timeline = (props) => {
+
+  return(
+    <TimelineEvent status={props.event.status} person={props.event.person} timeStamp={props.event.timeStamp}/>
+  );
 }
 
 export default Timeline;
