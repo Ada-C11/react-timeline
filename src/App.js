5add3180c8dbd8fecba2f02dbb576df1a69cf4db
Amy Wyatt <amy.wyatt12@outlook.com>
2019-06-10 15:57:42 -0700

    Timeline Event renders in application

--- src/App.js
+++ src/App.js
@@ -3,18 +3,18 @@ import './App.css';
 import timelineData from './data/timeline.json';
 
 import Timeline from './components/Timeline';
+import TimelineEvent from './components/TimelineEvent';
 
 class App extends Component {
   render() {
     console.log(timelineData);
-
-    // Customize the code below
     return (
       <div className="App">
         <header className="App-header">
-          <h1 className="App-title">Application title</h1>
+          <h1 className="App-title">Code Monkey</h1>
         </header>
         <main className="App-main">
+          <TimelineEvent />
         </main>
       </div>
     );
--- src/components/TimelineEvent.js
+++ src/components/TimelineEvent.js
@@ -2,9 +2,20 @@ import React from 'react';
 import './TimelineEvent.css';
 import Timestamp from './Timestamp';
 
-const TimelineEvent = () => {
-  // Fill in your code here
-  return;
+const TimelineEvent = (props) => {
+  return (
+    <section>
+      <h3>
+        Andrew
+      </h3>
+      <p>
+        I'm cool
+      </p>
+      <h5>
+        Now o'clock
+      </h5>
+    </section>
+  );
 }
 
 export default TimelineEvent;
