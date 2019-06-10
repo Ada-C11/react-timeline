8765da7ac8d4b53d296388e23fee7171e8f56713
amyesh <amyesh08@gmail.com>
2019-06-10 15:06:05 -0700

    added props and rendering capability to TimelineEvent and hardcoded data into the App function

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import timelineData from './data/timeline.json';
+import TimelineEvent from './components/TimelineEvent'
 
 import Timeline from './components/Timeline';
 
@@ -8,13 +9,13 @@ class App extends Component {
   render() {
     console.log(timelineData);
 
-    // Customize the code below
     return (
       <div className="App">
         <header className="App-header">
-          <h1 className="App-title">Application title</h1>
+          <h1 className="App-title">Ada Lovelace's social media feed</h1>
         </header>
         <main className="App-main">
+          <TimelineEvent person="Shel Silverstein" status="A treehouse, a free house, a secret you and me house, a high up in the leafy branches cozy as can be house."/>
         </main>
       </div>
     );
@@ -22,3 +23,5 @@ class App extends Component {
 }
 
 export default App;
+
+{/* <Student fullName="Improved Ada" email="improved-ada@ada.co" /> */}
--- src/components/TimelineEvent.js
+++ src/components/TimelineEvent.js
@@ -2,9 +2,16 @@ import React from 'react';
 import './TimelineEvent.css';
 import Timestamp from './Timestamp';
 
-const TimelineEvent = () => {
-  // Fill in your code here
-  return;
+const TimelineEvent = (props) => {
+  return (
+    <section>
+      <span><strong>{ props.person }</strong></span>
+      <span>{ <Timestamp /> }</span>
+      <p>
+        { props.status }
+      </p>
+    </section>
+  );
 }
 
 export default TimelineEvent;
