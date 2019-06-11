b9f9c2f27499026303e7f35b001695042df2de7d
K <774162+kaseea@users.noreply.github.com>
2019-06-11 10:48:05 -0700

    whelp this is where I am

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -1,10 +1,34 @@
 import React from 'react';
 import './Timeline.css';
 import TimelineEvent from './TimelineEvent';
+// import timelineData from './data/timeline.json';
 
-const Timeline = () => {
-  // Fill in your code here
-  return;
-}
+const Timeline = (props) => {
+  // although why do we have to do props.events? cause itsnt 
+  // <Timeline events={timelineData.events} /> already sending it events?
+  // side question, how would you debug that?
+
+  const timelineComponents = props.events.map( (timeline, i) => {
+    return (
+      <li key={i}>
+        <TimelineEvent
+          person={ timeline.person }
+          status={ timeline.status }
+          time={ timeline.timeStamp } />
+      </li>
+    );
+  });
+  
+    return (
+      <section>
+        <ul>
+          { timelineComponents }
+        </ul>
+      </section>
+    );
+  };
+
+
+
 
 export default Timeline;
--- src/components/TimelineEvent.js
+++ src/components/TimelineEvent.js
@@ -2,9 +2,19 @@ import React from 'react';
 import './TimelineEvent.css';
 import Timestamp from './Timestamp';
 
-const TimelineEvent = () => {
+const TimelineEvent = (props) => {
   // Fill in your code here
-  return;
+  return (
+    <section>
+
+    {/* <h3>{ props.timestamp } </h3> */}
+      <h3> <Timestamp time= { props.time }/></h3>
+      {/* so, note to self, it has to match the name time on Timestamp */}
+    <h3>
+      { props.person } , { props.status}
+    </h3>
+  </section>
+  );
 }
 
 export default TimelineEvent;
