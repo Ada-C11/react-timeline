cdc31f0f80a44ff344c676d2c8c4ffef5dbf53fb
Kate Sandler <kate@katekatekate.net>
2019-06-11 10:47:42 -0700

    Pushing all code up for review activity

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -2,9 +2,18 @@ import React from 'react';
 import './Timeline.css';
 import TimelineEvent from './TimelineEvent';
 
-const Timeline = () => {
-  // Fill in your code here
-  return;
-}
+const Timeline = props => {
+  const eventFeed = props.events.map((event, i) => {
+    return (
+      <TimelineEvent
+        key={i}
+        person={event.person}
+        status={event.status}
+        time={event.timeStamp}
+      />
+    );
+  });
 
+  return <section className="timeline">{eventFeed}</section>;
+};
 export default Timeline;
