84406c2eb1b9d8228bf027b0cf071f5d2e36c62b
Ngoc Le <ngocle@outlook.com>
2019-06-11 10:47:41 -0700

    added timeline event function

--- src/components/TimelineEvent.js
+++ src/components/TimelineEvent.js
@@ -2,9 +2,21 @@ import React from 'react';
 import './TimelineEvent.css';
 import Timestamp from './Timestamp';
 
-const TimelineEvent = () => {
-  // Fill in your code here
-  return;
+const TimelineEvent = (props) => {
+  
+  return (
+    <section>
+      <h4>
+        {props.person}
+      </h4>
+      <p>
+        <Timestamp 
+          time={props.timeStamp}
+        />
+      </p>
+      <p> {props.status}</p>
+    </section>
+  );
 }
 
 export default TimelineEvent;
