119fa0f136895dcbdeb244fcb226ae2346ba92c6
K <774162+kaseea@users.noreply.github.com>
2019-06-11 13:44:55 -0700

    just some more notes

--- src/App.js
+++ src/App.js
@@ -15,7 +15,9 @@ class App extends Component {
           <h1 className="App-title">{timelineData.person}</h1>
         </header>
         <main className="App-main">
-          <Timeline events={timelineData.events} />
+          {/* <Timeline events={timelineData["events"] person={timelineData.person}} />
+        sooo thats not how it works, just rememnber that events just equals something, ignore it, and we can pass in multipe props but theyre still a prop? I dunno but correctness would be... */}
+        <Timeline events={timelineData["events"]} />
         </main>
       </div>
     );
