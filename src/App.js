1227e8c6c4b9086b72d21f728352a7ffe8a5a15a
Kimberly-Fasbender <kimberly.fasbender@gmail.com>
2019-06-10 17:59:09 -0700

    added Ada profile photo

--- src/App.js
+++ src/App.js
@@ -15,11 +15,18 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
+          <img 
+            src='https://www.biography.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cg_face%2Cq_auto:good%2Cw_300/MTE4MDAzNDEwODQwOTQ2MTkw/ada-lovelace-20825279-1-402.jpg' 
+            alt="Ada Lovelace\'s avatar" />
           <h1 className="App-title">Ada Lovelace's Social Media Feed</h1>
         </header>
         <main className="App-main">
           <Timeline events={timelineData.events} />
         </main>
+
+        <footer>
+          kimbook, LLC
+        </footer>
       </div>
     );
   }
